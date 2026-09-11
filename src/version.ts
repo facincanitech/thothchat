8db155b16449efa8eba918c60b0eb2942c66b4c2
pezzarine <facincanitech@gmail.com>
2026-09-11 14:20:39 -0300

    Compact conversation list spacing

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.2.219"
-export const UPDATE_COUNT = 219
+export const APP_VERSION = "1.2.220"
+export const UPDATE_COUNT = 220
 export const APK_DOWNLOAD_URL = "https://github.com/facincanitech/thothchat/releases/download/latest/thothchat.apk"
